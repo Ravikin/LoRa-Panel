fb1009c48db0801200f70203b1ac3fe43561c05d
Ravikin <ravi@uwcontrol.pl>
2024-04-08 15:42:18 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -28,11 +28,11 @@ rec1 =  jsonObj["result"]["received_at"]
 
 st.header("LoRa Sensor Data")
 st.subheader(str(devices[1]))
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3 = st.columns(3)
 col1.metric(label="Temperature", value=temp1)
 col2.metric(label="Humidity", value=humi1)
 col3.metric(label="Battery Voltage", value=volt1)
-col4.metric(label="Last Seen", value=rec1)
+st.metric(label="Last Seen", value=rec1)
 
 
 
@@ -47,11 +47,11 @@ volt2 = jsonObj2["result"]["uplink_message"]["decoded_payload"]["volt"]
 rec2 =  jsonObj2["result"]["received_at"]
 
 st.subheader(str(devices[2]))
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3 = st.columns(3)
 col1.metric(label="Temperature", value=temp2)
 col2.metric(label="Humidity", value=humi2)
 col3.metric(label="Battery Voltage", value=volt2)
-col4.metric(label="Last Seen", value=rec2)
+st.metric(label="Last Seen", value=rec2)
 
 link = baseUrlDev+devices[0]+"/packages/storage/"+types[1]+"?limit=1"
 r0 = requests.get(link,headers=headers)
@@ -65,11 +65,11 @@ rec0 =  jsonObj0["result"]["received_at"]
 
 
 st.subheader(str(devices[0]))
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3 = st.columns(3)
 col1.metric(label="Temperature", value=temp0)
 col2.metric(label="Humidity", value=humi0)
 col3.metric(label="Battery Voltage", value=volt0)
-col4.metric(label="Last Seen", value=rec0)
+st.metric(label="Last Seen", value=rec0)
 
 
 data = {
