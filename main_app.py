7b34c1a129a5469b535ef9cf89faeb35e4a0fcc0
Ravikin <ravi@uwcontrol.pl>
2024-04-08 13:40:32 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -21,16 +21,16 @@ r = requests.get(link,headers=headers)
 
 jsonObj = json.loads(r.text)
 
-temp = jsonObj["result"]["uplink_message"]["decoded_payload"]["temp"]
-humi = jsonObj["result"]["uplink_message"]["decoded_payload"]["humi"]
-volt = jsonObj["result"]["uplink_message"]["decoded_payload"]["volt"]
+temp1 = jsonObj["result"]["uplink_message"]["decoded_payload"]["temp"]
+humi1 = jsonObj["result"]["uplink_message"]["decoded_payload"]["humi"]
+volt1 = jsonObj["result"]["uplink_message"]["decoded_payload"]["volt"]
 
 st.header("LoRa Sensor Data")
 st.subheader(str(devices[1]))
 col1, col2, col3 = st.columns(3)
-col1.metric(label="Temperature", value=temp)
-col2.metric(label="Humidity", value=humi)
-col3.metric(label="Battery Voltage", value=volt)
+col1.metric(label="Temperature", value=temp1)
+col2.metric(label="Humidity", value=humi1)
+col3.metric(label="Battery Voltage", value=volt1)
 
 
 
@@ -39,34 +39,51 @@ r = requests.get(link,headers=headers)
 
 jsonObj = json.loads(r.text)
 
-temp = jsonObj["result"]["uplink_message"]["decoded_payload"]["temp"]
-humi = jsonObj["result"]["uplink_message"]["decoded_payload"]["humi"]
-volt = jsonObj["result"]["uplink_message"]["decoded_payload"]["volt"]
+temp2 = jsonObj["result"]["uplink_message"]["decoded_payload"]["temp"]
+humi2 = jsonObj["result"]["uplink_message"]["decoded_payload"]["humi"]
+volt2 = jsonObj["result"]["uplink_message"]["decoded_payload"]["volt"]
 
 st.subheader(str(devices[2]))
 col1, col2, col3 = st.columns(3)
-col1.metric(label="Temperature", value=temp)
-col2.metric(label="Humidity", value=humi)
-col3.metric(label="Battery Voltage", value=volt)
+col1.metric(label="Temperature", value=temp2)
+col2.metric(label="Humidity", value=humi2)
+col3.metric(label="Battery Voltage", value=volt2)
 
 link = baseUrlDev+devices[0]+"/packages/storage/"+types[1]+"?limit=1"
 r = requests.get(link,headers=headers)
 
 jsonObj = json.loads(r.text)
 
-temp = jsonObj["result"]["uplink_message"]["decoded_payload"]["temp"]
-humi = jsonObj["result"]["uplink_message"]["decoded_payload"]["humi"]
-volt = "∞"
+temp0 = jsonObj["result"]["uplink_message"]["decoded_payload"]["temp"]
+humi0 = jsonObj["result"]["uplink_message"]["decoded_payload"]["humi"]
+volt0 = "∞"
 
 st.subheader(str(devices[0]))
 col1, col2, col3 = st.columns(3)
-col1.metric(label="Temperature", value=temp)
-col2.metric(label="Humidity", value=humi)
-col3.metric(label="Battery Voltage", value=volt)
+col1.metric(label="Temperature", value=temp0)
+col2.metric(label="Humidity", value=humi0)
+col3.metric(label="Battery Voltage", value=volt0)
 
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+data = {
+    'Location': [devices[0], devices[1], devices[2]],
+    'Latitude': [52.3634621, 52.3634621, 52.3634621],
+    'Longitude': [52.3634621, 52.3634621, 52.3634621],
+    'Humidity (%)': [humi0, humi1, humi2],
+    'Temperature (°C)': [temp0, temp1, temp2]
+}
 
-st.map(df)
+df = pd.DataFrame(data)
+
+# Sidebar inputs
+st.sidebar.header('Input Parameters')
+selected_point = st.sidebar.selectbox('Select a point:', df['Location'])
+selected_data = df[df['Location'] == selected_point].iloc[0]
+st.sidebar.write(f"Latitude: {selected_data['Latitude']}")
+st.sidebar.write(f"Longitude: {selected_data['Longitude']}")
+st.sidebar.write(f"Humidity: {selected_data['Humidity (%)']}%")
+st.sidebar.write(f"Temperature: {selected_data['Temperature (°C)']}°C")
+
+# Display the map
+st.header('Map with Points')
+st.map(df[['Latitude', 'Longitude']].values)
