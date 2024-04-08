994f3b72a4718e7b087a1ddf73935dbf1b66d1d7
Ravikin <ravi@uwcontrol.pl>
2024-04-08 14:05:19 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -70,9 +70,7 @@ data = {
     'latitude': [52.3632418, 52.3634742, 52.3622212],
     'longitude': [21.0507091, 21.0505278, 21.0497551],
     'Humidity (%)': [70, 80, 65],
-    'Temperature (°C)': [25, 28, 23],
-    'size':np.random.randn(1000) * 100,
-    'color':np.random.rand(1000, 4).tolist()
+    'Temperature (°C)': [25, 28, 23]
 }
 
 df = pd.DataFrame(data)
@@ -94,6 +92,5 @@ st.header('Map with Points')
 st.map(df,
     latitude='latitude',
     longitude='longitude',
-    size='size',
-    color='color')
+    size='Temperature (°C)')
 
