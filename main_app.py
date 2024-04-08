ff3495f5b4acd9e96a9a8489d86c54616382d6da
Ravikin <ravi@uwcontrol.pl>
2024-04-08 13:21:54 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import requests
 import json
 
-
-headers = {'Accept': 'application/json', 'Authorization':'Bearer NNSXS.KNEAMVSK4GGOAJ6ZQT5GF4GI44WFDHG6BMGLOZQ.COQXVOSUBLBX7HGVTYWXNZYODOV3V5O2Z5SSSUJES6CFWEP5JCHQ'}
+token = st.text_input("Bearer Token")
+bearer = "Bearer "+str(token)
+headers = {'Accept': 'application/json', 'Authorization':Bearer}
 devices = ["eui-2cf7f1205100a1e0","eui-mkrwan1310-1-a861","eui-a8610a3438306602","eui-2cf7f1205100b6a2-uno-main"]
 types =  [ "uplink_message", "uplink_normalized", "join_accept", "downlink_ack", "downlink_nack", "downlink_sent", "downlink_failed", "downlink_queued", "downlink_queue_invalidated", "location_solved", "service_data"]
 baseUrlAll="https://eu1.cloud.thethings.network/api/v3/as/applications/ravikin-test-app/packages/storage/{type}"
@@ -22,6 +23,7 @@ humi = jsonObj["result"]["uplink_message"]["decoded_payload"]["humi"]
 volt = jsonObj["result"]["uplink_message"]["decoded_payload"]["volt"]
 
 st.header("LoRa Sensor Data")
-st.metric(label="Temperature", value=temp)
-st.metric(label="Humidity", value=humi)
-st.metric(label="Battery Voltage", value=volt)
+col1, col2, col3 = st.columns(3)
+col1.metric(label="Temperature", value=temp)
+col2.metric(label="Humidity", value=humi)
+col3.metric(label="Battery Voltage", value=volt)
