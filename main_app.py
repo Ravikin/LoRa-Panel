8f5cb35f782fb3e32df1abf4aa45996e55ff6b57
Ravikin <ravi@uwcontrol.pl>
2024-04-08 12:58:45 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -3,9 +3,23 @@ import requests
 import json
 import pandas as pd
 import numpy as np
+import folium
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+headers = {'Accept': 'application/json', 'Authorization':'Bearer NNSXS.KNEAMVSK4GGOAJ6ZQT5GF4GI44WFDHG6BMGLOZQ.COQXVOSUBLBX7HGVTYWXNZYODOV3V5O2Z5SSSUJES6CFWEP5JCHQ'}
+devices = ["eui-2cf7f1205100a1e0","eui-mkrwan1310-1-a861","eui-a8610a3438306602","eui-2cf7f1205100b6a2-uno-main"]
+types =  [ "uplink_message", "uplink_normalized", "join_accept", "downlink_ack", "downlink_nack", "downlink_sent", "downlink_failed", "downlink_queued", "downlink_queue_invalidated", "location_solved", "service_data"]
+baseUrlAll="https://eu1.cloud.thethings.network/api/v3/as/applications/ravikin-test-app/packages/storage/{type}"
+baseUrlDev="https://eu1.cloud.thethings.network/api/v3/as/applications/ravikin-test-app/devices/"#{device_id}/packages/storage/{type}"
+limit = str(1)
 
-st.map(df)
+
+# link = "https://eu1.cloud.thethings.network/api/v3/as/applications/ravikin-test-app/packages/storage/uplink_normalized?limit="+limit
+link = baseUrlDev+devices[1]+"/packages/storage/"+types[1]+"?limit=1"
+r = requests.get(link,headers=headers)
+
+jsonObj = json.loads(r.text)
+
+temp = jsonObj["result"]["uplink_message"]["decoded_payload"]["temp"]
+humi = jsonObj["result"]["uplink_message"]["decoded_payload"]["humi"]
+volt = jsonObj["result"]["uplink_message"]["decoded_payload"]["volt"]
+
